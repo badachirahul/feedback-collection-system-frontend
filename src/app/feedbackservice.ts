2f5c3d0b5f5f4635a892ebfff5ef30ff5355a076
Rahul <146862969+badachirahul@users.noreply.github.com>
2025-07-26 23:04:20 +0530

    Update feedbackservice.ts

--- src/app/feedbackservice.ts
+++ src/app/feedbackservice.ts
@@ -8,8 +8,8 @@ export class Feedbackservice {
   
   constructor(private httpClient:HttpClient){}
 
-  backEndUrl = "http://localhost:8085/api/feedbacks";
-  backEndUrl1="http://localhost:8085/api/admin/feedbacks";
+  backEndUrl = "http://ec2-3-94-115-237.compute-1.amazonaws.com:8085/api/feedbacks";
+  backEndUrl1="http://ec2-3-94-115-237.compute-1.amazonaws.com:8085/api/admin/feedbacks";
 
   savefeedback(feedback:any)
   {
